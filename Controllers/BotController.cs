5172102f7375df82f97cadcb5fd1e7d869a5f58e
Daniel Evans <dev@microsoft.com>
2019-03-28 10:43:19 -0700

    Use the new streaming extension adapter!

--- Controllers/BotController.cs
+++ Controllers/BotController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core.StreamingExtensions;
 
 namespace Microsoft.Bot.Builder.EchoBot
 {
@@ -18,13 +19,13 @@ namespace Microsoft.Bot.Builder.EchoBot
         private readonly IBotFrameworkHttpAdapter _adapter;
         private readonly IBot _bot;
 
-        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        public BotController(BotFrameworkWebSocketAdapter adapter, IBot bot)
         {
             _adapter = adapter;
             _bot = bot;
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task PostAsync()
         {
             // Delegate the processing of the HTTP POST to the adapter.
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core.StreamingExtensions;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +40,9 @@ namespace Microsoft.Bot.Builder.EchoBot
             // Create the Bot Framework Adapter.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
+            // Create the Bot Framework Streaming Extension Adapter.
+            services.AddSingleton<BotFrameworkWebSocketAdapter>();
+
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, EchoBot>();
         }
@@ -57,7 +61,7 @@ namespace Microsoft.Bot.Builder.EchoBot
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
+            app.UseWebSockets();
             //app.UseHttpsRedirection();
             app.UseMvc();
         }
